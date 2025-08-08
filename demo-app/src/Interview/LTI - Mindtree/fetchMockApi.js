a081cff40089074d68028f62840611652acb8036
kabilanpersonal <kabilan0904@gmail.com>
2025-08-08 12:13:10 +0530

    Mindtree- questions

---
+++ demo-app/src/Interview/LTI - Mindtree/fetchMockApi.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+export default function FetchData() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        if (!res) {
+          throw new Error('HTTP error !!');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Data loading....</p>;
+  if (error) return <p>Error fetching Data : {error}</p>;
+
+  return (
+    <div>
+      <h2>User Data</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+// console.log([] == 0); //trues
+// console.log([] == false); //true
+// console.log([] + []); //""
+// console.log({} + []); //"[Object Object]"
