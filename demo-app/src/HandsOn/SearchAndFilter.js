cf9ac6bd7400931dd41c8b4b0cd7798f68f5491d
kabilanpersonal <kabilan0904@gmail.com>
2025-08-12 22:31:11 +0530

    debouncing addded

--- demo-app/src/HandsOn/SearchAndFilter.js
+++ demo-app/src/HandsOn/SearchAndFilter.js
@@ -1,32 +1,55 @@
-// Search and filter in the list
+import { useEffect, useState } from "react";
+
+// Search and filter in the list and debouncing it for 0.5 s
 function SearchAndFilter() {
-    const fruits = [
-      'Apple',
-      'Mango',
-      'Banana',
-      'Guava',
-      'Grapes',
-      'Chikku',
-      'Papaya',
-      'Pome',
-    ];
-  
+
     const [searchItem, setSearchItem] = useState('');
-    const filteredItem = fruits.filter((item) =>
-      item.toLowerCase().includes(searchItem.toLowerCase())
-    );
-  
+    const [result, setResult] = useState([]);
+
+    useEffect(() => {
+        if (!searchItem.trim()) {
+            setResult([]);
+            return;
+        }
+        const deBounced = setTimeout(() => {
+            fetchData(searchItem);
+        }, 500);
+
+        return () => clearTimeout(deBounced)
+    }, [searchItem])
+
+    const fetchData = async search => {
+        const fruits = [
+            'Apple',
+            'Mango',
+            'Banana',
+            'Guava',
+            'Grapes',
+            'Chikku',
+            'Papaya',
+            'Pome',
+        ];
+        const filteredItem = fruits.filter((item) =>
+            item.toLowerCase().includes(searchItem.toLowerCase())
+        );
+
+        setResult(filteredItem)
+
+    }
+
+
+
     return (
-      <>
-        <h3>Searching and Filtering Elements in the List</h3>
-        <input type="text" placeholder="SearchItem" value = {searchItem} onChange = {(e)=> setSearchItem(e.target.value)}/>
-  
-        <ul>
-          {filteredItem.length > 0 ? (filteredItem.map((item,i) => (
-            <li key={i}>{item}</li>
-          ) )) : (<li>No Records Found</li>) }
-        </ul>
-      </>
+        <>
+            <h3>Searching and Filtering Elements in the List</h3>
+            <input type="text" placeholder="SearchItem" value={searchItem} onChange={(e) => setSearchItem(e.target.value)} />
+
+            <ul>
+                {result.length > 0 ? (result.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))) : (<li>No Records Found</li>)}
+            </ul>
+        </>
     );
-  }
-  export default SearchAndFilter;
+}
+export default SearchAndFilter;
