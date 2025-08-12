74051719749e8455716b1dea76a1bcb81cd29b9c
kabilanpersonal <kabilan0904@gmail.com>
2025-08-12 19:59:49 +0530

    format

--- demo-app/src/HandsOn/ParallelApiCalls.js
+++ demo-app/src/HandsOn/ParallelApiCalls.js
@@ -1,43 +1,44 @@
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 
-export default function ParallelApiCalls(){
-    const [userData, setUserData]= useState([]);
-    const [postData, setpostData]= useState([]);
+export default function ParallelApiCalls() {
+    const [userData, setUserData] = useState([]);
+    const [postData, setpostData] = useState([]);
     const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [error, setError] = useState(null);
 
-  useEffect(()=>{
-    const fetchUser = fetch('https://jsonplaceholder.typicode.com/users').then(res=> res.json());
-    const postUser = fetch('https://jsonplaceholder.typicode.com/posts').then(res=> res.json());
+    useEffect(() => {
+        const fetchUser = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+        const postUser = fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json());
 
-    Promise.all([fetchUser,postUser])
-        .then(([userData,postData])=>{
-            setUserData(userData.slice(0,5));
-            setpostData(postData.slice(0,5));
-            setLoading(false);
-        })
-        .catch((error)=>{
-            setError(error);
-            setLoading(false)
-        })
-  },[]);
-  if (loading) return <p>Loading data...</p>;
-  if (error) return <p>{error}</p>;
+        Promise.all([fetchUser, postUser])
+            .then(([userData, postData]) => {
+                setUserData(userData.slice(0, 5));
+                setpostData(postData.slice(0, 5));
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error);
+                setLoading(false)
+            })
+    }, []);
+    if (loading) return <p>Loading data...</p>;
+    if (error) return <p>{error}</p>;
 
-  return (
-    <div>
-        <h3>ParallelApiCalls</h3>
-        <ul>
-            {userData.map(data=>(
-                <li key={data.id}> {data.name}-{data.email}</li>
-            ))}
-        </ul>
-        <ul>
-            {postData.map(data=>(
-                <li key={data.id}> {data.title}</li>
-            ))}
-        </ul>
-    </div>
-  )
+    return (
+        <div>
+            <h3>ParallelApiCalls</h3>
+            <ul>
+                {userData.map(data => (
+                    <li key={data.id}> {data.name}-{data.email}</li>
+                ))}
+            </ul>
+            <ul>
+                {postData.map(data => (
+                    <li key={data.id}> {data.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
 
 }
+
